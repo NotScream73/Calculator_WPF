880cf58f9bfe546453ce421d9c41f5c1966d2b8c
Nikita Sergeev <notscreamplay73@gmail.com>
2023-02-21 21:01:41 +0400

    Ресурсы приложения, команды. Создание ресурсов и их использование, подключение команды к элементам управления.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,62 @@ namespace Calculator
         public MainWindow()
         {
             InitializeComponent();
+            foreach (var el in root.Children)
+            {
+                if (el is Button)
+                {
+                    if ((string)((Button)el).Content != "=")
+                        ((Button)el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = textBox1.Text == "Error" ? "" : textBox1.Text;
+            var str = (string)((Button)sender).Content;
+            if (str == "C")
+            {
+                textBox1.Text = "";
+            }
+            else if (str == "→")
+            {
+                var temp = textBox1.Text.Length;
+                if (temp >= 2)
+                {
+                    textBox1.Text = textBox1.Text.Substring(0, temp - 1);
+                }
+                else if (temp <= 1)
+                {
+                    textBox1.Text = "";
+                }
+            }
+            else
+            {
+                textBox1.Text += str;
+            }
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(textBox1.Text))
+                    throw new Exception();
+                var value = new DataTable().Compute(textBox1.Text, null).ToString();
+                textBox1.Text = value;
+                MessageBox.Show("Успешный подсчёт, ты молодец!");
+            }
+            catch
+            {
+                MessageBox.Show("Где-то ты ошибься!");
+                textBox1.Text = "Error";
+            }
         }
     }
 }
